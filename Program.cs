91f7ba91496c504d36c17bc778df2e9a6739d057
TarynL <tarlytle@gmail.com>
2021-05-21 15:23:42 -0500

    magic moose complete

--- Program.cs
+++ Program.cs
@@ -42,7 +42,52 @@ void MooseSays(string message)
     ");
 }
 
-bool MooseAsks(string question)
+
+string[] responses = {
+    "As I see it, yes.",
+"Ask again later.",
+"Better not tell you now.",
+"Cannot predict now.",
+"Concentrate and ask again.",
+"Don’t count on it.",
+"It is certain.",
+"It is decidedly so.",
+"Most likely.",
+"My reply is no.",
+"My sources say no.",
+"Outlook not so good.",
+"Outlook good.",
+"Reply hazy, try again.",
+"Signs point to yes.",
+"Very doubtful.",
+"Without a doubt.",
+"Yes.",
+"Yes – definitely.",
+"You may rely on it."
+};
+
+MagicMoose();
+
+void MagicMoose()
+{
+    MooseSays("Hey there cutie! What would you like to know?");
+    string answer = Console.ReadLine().ToLower();
+    if (string.IsNullOrEmpty(answer))
+    {
+        MooseSays("That is ok. You must have all the answers. Have a nice day!");
+    }
+    else
+    {
+        Random r = new Random();
+        int genRand = r.Next(0, responses.Length - 1);
+        MooseSays(responses[genRand]);
+        MagicMoose();
+    }
+
+}
+
+
+/*bool MooseAsks(string question)
 {
     Console.Write($"{question} (Y/N): ");
     string answer = Console.ReadLine().ToLower();
@@ -79,7 +124,7 @@ void MooseQ(string question, string TrueResponse, string FalseResponse)
     {
         MooseSays(FalseResponse);
     }
-}
+}*/
 
 // Main();
 
